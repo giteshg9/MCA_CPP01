8a3727eb8ef2104a40d27fa44350a4c316c4ad8f
giteshg9 <giteshg2000@gmail.com>
2024-06-11 11:43:46 +0530

    both file

--- function.cpp
+++ function.cpp
@@ -1,17 +1,18 @@
 #include <iostream>
 using namespace std;
 
-void myFunc2()
-{
-    cout << "2" << endl;
-}
+// void myFunc2()
+// {
+//     cout << "2" << endl;
+// }
 
-void myFunc()
+void myFunc(string name, int age)
 {
+    cout << "Name is : " << name << "age is : " << age<<endl ;
     cout << "1" << endl;
 }
 int main()
 {
-    myFunc2();
-    myFunc();
+    //myFunc2();
+    myFunc("GItesh",23);
 }
