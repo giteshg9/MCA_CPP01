4d403a628c67f6f5562ef3cd8ef92bdafa2535b2
giteshg9 <giteshg2000@gmail.com>
2024-06-13 11:07:11 +0530

    working

--- oop/pracClass.cpp
+++ oop/pracClass.cpp
@@ -3,11 +3,12 @@ using namespace std;
 
 class student
 {
+    public:
     string name;
     int age;
     int rollno;
 
-    void show(string name,int age,int rollno)
+    void show()
     {
         cout << "Name of the student is : " << name << endl;
         cout << "Age of the student is : " << age << endl;
@@ -17,5 +18,11 @@ class student
 
 int main()
 {
-    
+    student s1;
+    s1.name="Fish";
+    s1.age = 5;
+    s1.rollno = 442201;
+
+    s1.show();
+    return 0;
 }
