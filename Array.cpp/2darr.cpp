5478319997b8363e2e61e757cbb701d06fa70808
giteshg9 <giteshg2000@gmail.com>
2024-06-27 11:55:23 +0530

    pattern :(

---
+++ Array.cpp/2darr.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[3][4];
+    /*for (int row = 0; row < 3; row++)
+    {
+        for(int col = 0;col < 4; col++)
+        {
+            cin >> arr[row][col];
+        }
+    }*/
+   for (int col = 0; col < 3; col++)
+    {
+        for(int row = 0;row < 4; row++)
+        {
+            cin >> arr[col][row];
+        }
+
+    /*for (int row = 0; row < 3; row++)
+    {
+        for(int col = 0;col < 4; col++)
+        {
+            cout << arr[row][col];
+        }
+    }
+    }*/
+    for (int col = 0; col < 3; col++)
+    {
+        for(int row = 0;row < 4; row++)
+        {
+            cin >> arr[col][row];
+        }
+}}}
---
+++ patttern/2dpattern.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+void wavePrint(int arr[][4],int row,int col)
+{
+        for (int col = 0; col < 4; col++)
+        {
+            if(col%2 == 1)
+            {
+            for (int row = row - 1; row >=0; row++)
+            {
+                cout<<endl;
+            }
+        }
+    else
+        for(row = row - 1,>=0,r--)
+        {
+            cout<<endl;
+        }    
+    }
+    
+}
+
+int main()
+{
+    int arr[][3];
+
+}
